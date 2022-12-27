e71d867719c6bb077907ceaa2b9c93e35fbd96b0
Peter Becich <peterbecich@gmail.com>
2022-12-26 19:35:54 -0800

    disable evalSystem

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
                        hixProject =
                          final.haskell-nix.hix.project {
                            src = ./.;
-                           evalSystem = "x86_64-linux";
+                           # evalSystem = "x86_64-linux";
                          };
                      })
                    ];
