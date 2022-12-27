782e95a4a68afb31c0317dbeb8ab6838ba62b78c
Peter Becich <peterbecich@gmail.com>
2022-12-26 19:07:53 -0800

    more inputs to find Mac error

--- flake.nix
+++ flake.nix
@@ -3,9 +3,11 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system: {
 
       packages.default = nixpkgs.legacyPackages.${system}.hello;
