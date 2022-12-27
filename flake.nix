17819fb36076cced8f0084903dee696c88bd2bbe
Peter Becich <peterbecich@gmail.com>
2022-12-26 19:35:08 -0800

    clean-up

--- flake.nix
+++ flake.nix
@@ -4,26 +4,17 @@
   inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = { self, nixpkgs, flake-utils, haskellNix }:
-    let
-      supportedSystems = [
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
-    in
-      # flake-utils.lib.eachSystem supportedSystems (system:
-      flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ haskellNix.overlay
-          (final: prev: {
-            hixProject =
-              final.haskell-nix.hix.project {
-                src = ./.;
-                evalSystem = "x86_64-linux";
-              };
-          })
-        ];
+                     (final: prev: {
+                       hixProject =
+                         final.haskell-nix.hix.project {
+                           src = ./.;
+                           evalSystem = "x86_64-linux";
+                         };
+                     })
+                   ];
         pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
         flake = pkgs.hixProject.flake {};
       in flake // {
